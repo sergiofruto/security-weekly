d1c5fc502313301bb5238d4a0fac1e82ca55266e
Sergio Fruto <sergio.fruto@auth0.com>
2016-07-26 11:50:15 -0300

    navigation component created

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -3,12 +3,14 @@ import Header from './Header';
 import Footer from './Footer';
 import SubscribeForm from './SubscribeForm';
 import ArticlesContainer from './ArticlesContainer';
+import Navigation from './Navigation';
 
 var App = () =>
   <div className="container">
     <Header></Header>
     <SubscribeForm></SubscribeForm>       
     <ArticlesContainer></ArticlesContainer>
+    <Navigation></Navigation>
     <Footer></Footer>
   </div>;
 
---
+++ scripts/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+var Navigation = () =>
+  <nav>
+    <ul className="nav-list">
+      <li><a href="">May 31</a></li>
+      <li><a href="">June 7</a></li>
+      <li><a href="">June 14</a></li>
+      <li><a href="">June 21</a></li>
+      <li><a href="">June 28</a></li>
+    </ul>
+  </nav>
+
+export default Navigation;
